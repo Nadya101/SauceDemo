ed83492c026592c92eaaa236138782f1a83a384d
Nadya.Bryashchey <angel.nadii36@gmail.com>
2021-11-24 15:57:42 +0200

    Added circleCI

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,46 @@
+package pages;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    private static final By USERNAME_INPUT = By.xpath("//*[@id='user-name']");
+    private static final By PASSWORD_INPUT = By.xpath("//*[@id='password']");
+    private static final By LOGIN_BUTTON = By.xpath("//*[@id='login-button']");
+    private static final By ERROR_MESSAGE = By.xpath("//*[@data-test = 'error']");
+
+    public LoginPage openPage() {
+        openPage(BASE_URL);
+        waitForPageLoaded();
+        return this;
+    }
+
+
+
+    @Step("Login as username: '{username}' and password: '{password}'")
+    public ProductsPage login(String username, String password) {
+        driver.findElement(USERNAME_INPUT).sendKeys(username);
+        driver.findElement(PASSWORD_INPUT).sendKeys(password);
+        clickOnLoginButton();
+        return new ProductsPage(driver);
+    }
+
+    @Step("Click on 'Login' button on Login page")
+    public void clickOnLoginButton(){
+        driver.findElement(LOGIN_BUTTON).click();
+    }
+
+    public String getErrorText() {
+        return driver.findElement(ERROR_MESSAGE).getText();
+    }
+
+    public String getUrl(){
+        return  driver.getCurrentUrl();
+    }
+}
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -8,20 +8,20 @@ import org.testng.annotations.BeforeMethod;
 import pages.CartPage;
 import pages.CheckoutPage;
 import pages.ProductsPage;
-import pages.LoginPage2;
+import pages.LoginPage;
 
 public class BaseTest {
     WebDriver driver;
     ProductsPage productsPage;
     CartPage cartPage;
     CheckoutPage checkoutPage;
-    LoginPage2 loginPage;
+    LoginPage loginPage;
 
     @BeforeMethod
     public void initTest() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        loginPage = new LoginPage2(driver);
+        loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
         checkoutPage = new CheckoutPage(driver);
